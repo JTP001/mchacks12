3c5c3995feeb19467b4e5632db8ab79cf26b97d3
mondaydavid <mondaydevid@gmail.com>
2025-01-25 21:47:33 -0500

    here

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import InputBox from './components/InputBox';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Text Saver App</h1>
+        <InputBox />
+      </header>
+    </div>
+  );
+}
+
+export default App;
